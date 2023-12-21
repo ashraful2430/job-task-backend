7a1ae7b8b20914a1b79088f044bcd95943e62072
ashraful2430 <www.savage582@gmail.com>
2023-12-21 20:50:13 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@cluster0.cy95lx0.mongodb.net/?retryWrites=true&w=majority`;
@@ -21,6 +27,26 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const userCollection = client.db("jobTaskDb").collection("users");
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.post("/new-users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existedUser = await userCollection.findOne(query);
+      if (existedUser) {
+        return res.send({ message: "user already existed", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
@@ -32,8 +58,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("job task  server is running");
+  res.send("job task is running");
 });
+
 app.listen(port, () => {
-  console.log(`job task server is running of ${port}`);
+  console.log(`job task in running ${port}`);
 });
