59b742dc5fcdc8fb2940711c407fe5ab553b0f78
ashraful2430 <www.savage582@gmail.com>
2023-12-22 02:29:44 +0600

    created get api to load data based on the staus

--- index.js
+++ index.js
@@ -43,6 +43,24 @@ async function run() {
       const result = await taskCollection.find(query).toArray();
       res.send(result);
     });
+    app.get("/tasks/:email/to-do", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email, status: "to-do" };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/tasks/:email/ongoing", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email, status: "ongoing" };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/tasks/:email/completed", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email, status: "completed" };
+      const result = await taskCollection.find(query).toArray();
+      res.send(result);
+    });
     app.post("/tasks", async (req, res) => {
       const info = req.body;
       const result = await taskCollection.insertOne(info);
