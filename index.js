4187ff41abd4c877e29ab44e92519b511d76df8a
ashraful2430 <www.savage582@gmail.com>
2023-12-22 14:27:14 +0600

    created api to load specific data based on status

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -37,30 +37,42 @@ async function run() {
       const result = await taskCollection.find().toArray();
       res.send(result);
     });
+
     app.get("/tasks/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const result = await taskCollection.find(query).toArray();
       res.send(result);
     });
+
     app.get("/tasks/:email/to-do", async (req, res) => {
       const email = req.params.email;
       const query = { email: email, status: "to-do" };
       const result = await taskCollection.find(query).toArray();
       res.send(result);
     });
+
     app.get("/tasks/:email/ongoing", async (req, res) => {
       const email = req.params.email;
       const query = { email: email, status: "ongoing" };
       const result = await taskCollection.find(query).toArray();
       res.send(result);
     });
+
     app.get("/tasks/:email/completed", async (req, res) => {
       const email = req.params.email;
       const query = { email: email, status: "completed" };
       const result = await taskCollection.find(query).toArray();
       res.send(result);
     });
+
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.post("/tasks", async (req, res) => {
       const info = req.body;
       const result = await taskCollection.insertOne(info);
